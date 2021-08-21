f1e75a3e0fc53247cc2ddb5ae26bb7dd77424a68
kimnewbie <gracecancho@gmail.com>
2021-08-21 22:19:08 +0900

    changed my favicon

---
+++ src/components/Login.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+export default function Login() {
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const onSubmitForm = (e) => {
+        e.preventDefault(); // submit 하면 화면이 사라져서
+        console.log(userName, password)
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmitForm}>
+                <input
+                    placeholder="Username"
+                    value={userName}
+                    onChange={(e)=>setUserName(e.target.value)}
+                /><br />
+                <input
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
+                /><br />
+                <button type="submit">LogIn</button>
+            </form>
+        </div>
+    )
+}
