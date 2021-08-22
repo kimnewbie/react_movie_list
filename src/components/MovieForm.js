6d2aede29749a185a6d08805d1ba2d6a4348e425
kimnewbie <gracecancho@gmail.com>
2021-08-22 17:41:17 +0900

    error message

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,22 +1,43 @@
 import React, {useState} from 'react'
 
-export default function MovieForm({onSubmitAddMovie}) { 
+export default function MovieForm({onSubmitAddMovie}) {
 
     const [movieTitle, setMovieTitle] = useState('');
     const [movieYear, setMovieYear] = useState('');
+    const [titleError, setTitleError] = useState('');
+    const [yearError, setYearError] = useState('');
+
     const resetForm = () => {
         setMovieTitle('');
         setMovieYear('');
     };
 
+    const validateForm = () => {
+        let validated = true;
+
+        if (!movieTitle) {
+            setTitleError('영화제목을 넣어주세요');
+            validated = false;
+        }
+        if (!movieYear) {
+            setYearError('개봉년도를 넣어주세요');
+            validated = false;
+        }
+        return validated;
+    }
+
+    const resetErrors = () => {
+        setTitleError('');
+        setYearError('');
+    }
+
     const onSubmit = (e) => {
         e.preventDefault();
-        onSubmitAddMovie({
-            id: Date.now(),
-            title: movieTitle,
-            year: movieYear
-        }) 
-        resetForm();
+        if (validateForm()) {
+            onSubmitAddMovie({id: Date.now(), title: movieTitle, year: movieYear});
+            resetErrors();
+            resetForm();
+        }
     }
 
     return (
@@ -26,11 +47,13 @@ export default function MovieForm({onSubmitAddMovie}) {
                 value={movieTitle}
                 placeholder="영화 제목"
                 onChange={e => setMovieTitle(e.target.value)}/><br/>
+            <div className="error">{titleError}</div>
             <input
-                type="text"
+                type="number"
                 value={movieYear}
                 placeholder="개봉 년도"
                 onChange={e => setMovieYear(e.target.value)}/><br/>
+            <div className="error">{yearError}</div>
             <button type="submit">영화 추가</button>
         </form>
     )
