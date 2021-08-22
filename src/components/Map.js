813907278918de5ddc26434b34be524ccc71d2c2
kimnewbie <gracecancho@gmail.com>
2021-08-22 14:38:50 +0900

    children changes parents component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react'
+import MovieForm from './MovieForm';
 import Movies from './Movies';
 
 export default function Map() {
@@ -15,45 +16,21 @@ export default function Map() {
         }
     ]);
 
-    const [movieTitle, setMovieTitle] = useState('');
-    const [movieYear, setMovieYear] = useState('');
-
     const renderMovies = movies.map((movie, index) => {
         return (<Movies movie={movie} key={index}/>);
     })
 
-    const onSubmitAddMovie = (e) => {
-        e.preventDefault();
-        // movies.push({ title: movieTitle, year: movieYear })
+    const onSubmitAddMovie = (movie) => { // movie 객체 받아오기
         setMovies([
             ...movies, 
-            {
-                title: movieTitle,
-                year: movieYear
-            }
-        ])
-        setMovieTitle('');
-        setMovieYear('');
+            movie
+        ]) 
     }
-
-    useEffect(() => {}, [])
 
     return (
         <div>
             <h1>Movie List</h1>
-            <form onSubmit={onSubmitAddMovie}>
-                <input
-                    type="text"
-                    value={movieTitle}
-                    placeholder="영화 제목"
-                    onChange={e => setMovieTitle(e.target.value)}/><br/>
-                <input
-                    type="text"
-                    value={movieYear}
-                    placeholder="개봉 년도"
-                    onChange={e => setMovieYear(e.target.value)}/><br/>
-                <button type="submit">영화 추가</button>
-            </form>
+            <MovieForm onSubmitAddMovie={onSubmitAddMovie} />
             {renderMovies}
         </div>
     )
---
+++ src/components/MovieForm.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+
+export default function MovieForm({onSubmitAddMovie}) { 
+
+    const [movieTitle, setMovieTitle] = useState('');
+    const [movieYear, setMovieYear] = useState('');
+    const resetForm = () => {
+        setMovieTitle('');
+        setMovieYear('');
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        onSubmitAddMovie({
+            title: movieTitle,
+            year: movieYear
+        }) 
+        resetForm();
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                type="text"
+                value={movieTitle}
+                placeholder="영화 제목"
+                onChange={e => setMovieTitle(e.target.value)}/><br/>
+            <input
+                type="text"
+                value={movieYear}
+                placeholder="개봉 년도"
+                onChange={e => setMovieYear(e.target.value)}/><br/>
+            <button type="submit">영화 추가</button>
+        </form>
+    )
+}
