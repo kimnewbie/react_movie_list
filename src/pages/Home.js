d218f9cac0bec43492b0be70232b550173866c72
kimnewbie <gracecancho@gmail.com>
2021-08-23 23:32:18 +0900

    tried tabs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Tabs, Tab, AppBar} from '@material-ui/core';
+import About from '.././materialUi/About';
+import Contact from '.././materialUi/Contact';
 
 export default function Home() {
     const [selectedTab, setSelectedTab] = useState(0);
+    const handleChange = (e, newValue) => {
+        setSelectedTab(newValue);
+    }
     return (
-        <div>
-             <h1>Home</h1>
-        </div>
+        <>
+            <AppBar position="static">
+                <Tabs value={selectedTab} onChange={handleChange}>
+                    <Tab label="About"/>
+                    <Tab label="Contact Us"/>
+                </Tabs>
+            </AppBar>
+            {setSelectedTab === 0 && <About />}
+            {setSelectedTab === 1 && <Contact />}
+        </>
     )
 }
