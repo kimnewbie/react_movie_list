85097c60e3ccdd64bf511383f55e12256e4f130c
kimnewbie <gracecancho@gmail.com>
2021-08-23 23:41:19 +0900

    rollback

--- src/materialUi/Contact.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Contact = () => {
-  return <div>This is the contact tab</div>;
-};
-
-export default Contact;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,9 @@
-import React, {useState} from 'react'
-import {Tabs, Tab, AppBar} from '@material-ui/core';
-import About from '.././materialUi/About';
-import Contact from '.././materialUi/Contact';
+import React from 'react'
 
 export default function Home() {
-    const [selectedTab, setSelectedTab] = useState(0);
-    const handleChange = (e, newValue) => {
-        setSelectedTab(newValue);
-    }
     return (
-        <>
-            <AppBar position="static">
-                <Tabs value={selectedTab} onChange={handleChange}>
-                    <Tab label="About"/>
-                    <Tab label="Contact Us"/>
-                </Tabs>
-            </AppBar>
-            {setSelectedTab === 0 && <About />}
-            {setSelectedTab === 1 && <Contact />}
-        </>
+        <div>
+             <h1>Home</h1>
+        </div>
     )
 }
